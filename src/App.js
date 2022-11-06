75374e0a06803db7609f4f78b6260ad63328abb2
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-11-06 18:56:35 -0500

    Add pages home, list and hotel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>App</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/hotels" element={<List />} />
+				<Route path="hotels/:id" element={<Hotel />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
