d2b05dd6997ad615cc9bee9666b771ea446f434e
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-11-08 19:42:39 -0500

    Add home page components
    Featured, featuredProperties, mailList, porpertyList and footer components are added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Featured from "../../components/Featured/Featured";
+import PropertyList from "../../components/PropertyList/PropertyList";
+import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties";
+import MediaList from "../../components/MailList/MailList";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import "./home.css";
@@ -8,6 +13,14 @@ const Home = () => {
 		<div>
 			<Navbar />
 			<Header />
+			<div className="homeContainer">
+				<Featured />
+				<h1 className="homeTitle">Browse by property type</h1>
+				<PropertyList />
+				<FeaturedProperties />
+				<MediaList />
+				<Footer />
+			</div>
 		</div>
 	);
 };
