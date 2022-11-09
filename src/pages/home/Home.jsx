3e3b5ef4daa6fd2190bc127cec9e4cc8352b33ef
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-11-08 22:09:36 -0500

    Add a new component SearchItem and fix error home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Featured from "../../components/Featured/Featured";
 import PropertyList from "../../components/PropertyList/PropertyList";
 import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties";
-import MediaList from "../../components/MailList/MailList";
+import MailList from "../../components/MailList/MailList";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
@@ -17,8 +17,9 @@ const Home = () => {
 				<Featured />
 				<h1 className="homeTitle">Browse by property type</h1>
 				<PropertyList />
+				<h1 className="homeTitle">Homes guests love</h1>
 				<FeaturedProperties />
-				<MediaList />
+				<MailList />
 				<Footer />
 			</div>
 		</div>
