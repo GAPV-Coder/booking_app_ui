5ce0dfae8b358eeb860949e9267e50a514252b91
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-12-27 19:40:11 -0500

    Update endpoint login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,10 @@ const Login = () => {
 		dispatch({ type: "LOGIN_START" });
 
 		try {
-			const res = await axios.post("/auth/login", credentials);
+			const res = await axios.post(
+				"http://localhost:8800/api/auth/login",
+				credentials
+			);
 			dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
 			navigate("/");
 		} catch (err) {
